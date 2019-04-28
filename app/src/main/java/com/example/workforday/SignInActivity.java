dfc91e269e2fd4beebee2b11d8dd02408ff9add9
Andrew <andreykuruch794@gmail.com>
2019-04-28 11:44:49 +0300

    Sign in Activity is added etc.

--- app/src/main/java/com/example/workforday/SignInActivity.java
+++ app/src/main/java/com/example/workforday/SignInActivity.java
@@ -1,4 +1,33 @@
 package com.example.workforday;
 
-public class SignInActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        TextView textView = findViewById(R.id.id_sing_up_text_view);
+
+        EditText email = findViewById(R.id.id_email_edit_text);
+        EditText password = findViewById(R.id.id_password_edit_text);
+
+        ImageButton imageButton = findViewById(R.id.id_close_sign_in);
+
+        imageButton.setOnClickListener(v -> finish());
+
+         textView.setOnClickListener(v -> {
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+        });
+    }
 }
